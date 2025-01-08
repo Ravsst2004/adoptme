b8a505e3e88367bb435e9d85857d93df78927728
Ravindra <128573488+Ravsst2004@users.noreply.github.com>
2025-01-08 13:25:43 +0800

    feat: update data

--- server/routes/web.php
+++ server/routes/web.php
@@ -3,9 +3,7 @@
 use App\Models\Pet;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    $pet = Pet::find(13);
-    return view('welcome', compact('pet'));
-});
-
-// Route::delete('/pets/{pet}', [App\Http\Controllers\PetController::class, 'destroy'])->name('pets.destroy');
+// Route::get('/', function () {
+//     $pet = Pet::find(13);
+//     return view('welcome', compact('pet'));
+// });
